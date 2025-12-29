d26c07191d7bcd7e86139321982c6c13a30f9f73
greepypastaid <gangsar.reka@gmail.com>
2025-12-29 16:02:12 +0700

    feat: wpu ci4 ep5 view

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -6,22 +6,44 @@ class Pages extends BaseController
 {
     public function index()
     {
-        echo view("/layouts/header");
-        echo view("/pages/home");
-        echo view("/layouts/footer");
+        $data = [
+            'title' => 'Home | WPU'
+        ];
+        echo view("layouts/header", $data);
+        echo view("pages/home");
+        echo view("layouts/footer");
     }
 
     public function about()
     {
-        echo view("/layouts/header");
-        echo view("/pages/about");
-        echo view("/layouts/footer");
+        $data = [
+            'title' => 'About Me'
+        ];
+        echo view("layouts/header", $data);
+        echo view("pages/about");
+        echo view("layouts/footer");
     }
 
     public function contact()
     {
-        echo view("/layouts/header");
-        echo view("/pages/contact");
-        echo view("/layouts/footer");
+        $data = [
+            'title' => 'Contact Us',
+            'alamat' => [
+                [
+                    'tipe' => 'Rumah',
+                    'alamat' => 'Jl. abc No. 123',
+                    'kota' => 'Bandung'
+                ],
+                [
+                    'tipe' => 'Kantor',
+                    'alamat' => 'Jl. Setiabudi No. 193',
+                    'kota' => 'Bandung'
+                ]
+            ]
+        ];
+
+        echo view("layouts/header", $data);
+        echo view("pages/contact", $data);
+        echo view("layouts/footer");
     }
 }
--- app/Views/pages/contact.php
+++ app/Views/pages/contact.php
@@ -1,10 +1,14 @@
-<div class="p-3">
-    <h1>Contact</h1>
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quos voluptas harum molestias reprehenderit
-        dolor, quidem ipsam. Beatae odio maxime accusantium a culpa. Ipsum quisquam perspiciatis, enim sint ut fugit!
-    </p>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
-        crossorigin="anonymous"></script>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h2>Contact Us</h2>
+            <?php foreach ($alamat as $a) : ?>
+                <ul>
+                    <li><?= $a['tipe']; ?></li>
+                    <li><?= $a['alamat']; ?></li>
+                    <li><?= $a['kota']; ?></li>
+                </ul>
+            <?php endforeach; ?>
+        </div>
+    </div>
 </div>
